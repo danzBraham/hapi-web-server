b21704e9fb5b09e7c3f92ec1eb0f60b368a04681
Zidan Abraham <zidangit07@gmail.com>
2023-12-26 12:01:33 +0800

    refactor: refactor server & routes

--- routes.js
+++ routes.js
@@ -1,88 +1,78 @@
 const routes = [
-   {
-      method: "GET",
-      path: "/",
-      handler: (request, h) => {
-         return "Homepage";
-      },
-   },
+  {
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return 'Homepage';
+    },
+  },
+  {
+    method: '*',
+    path: '/',
+    handler: (request, h) => {
+      const { method } = request;
+      return `Halaman tidak dapat diakses dengan ${method} request`;
+    },
+  },
+  {
+    method: 'GET',
+    path: '/about',
+    handler: (request, h) => {
+      return 'About Page';
+    },
+  },
+  {
+    method: '*',
+    path: '/about',
+    handler: (request, h) => {
+      const { method } = request;
+      return `Halaman tidak dapat diakses dengan ${method} request`;
+    },
+  },
+  {
+    method: '*',
+    path: '/hello/{name?}',
+    handler: (request, h) => {
+      const { name = 'stranger' } = request.params;
+      const { lang } = request.query;
 
-   {
-      method: "*",
-      path: "/",
-      handler: (request, h) => {
-         const {method} = request;
-         return `Halaman tidak dapat diakses dengan ${method} request`;
-      },
-   },
+      if (lang === 'id') return `Halo ${name}!`;
+      if (lang === 'jp') return `Ohayou ${name}!`;
+      if (lang === 'fr') return `Bonjour ${name}!`;
 
-   {
-      method: "GET",
-      path: "/about",
-      handler: (request, h) => {
-         return "About Page";
-      },
-   },
-
-   {
-      method: "*",
-      path: "/about",
-      handler: (request, h) => {
-         const {method} = request;
-         return `Halaman tidak dapat diakses dengan ${method} request`;
-      },
-   },
-
-   {
-      method: "*",
-      path: "/hello/{name?}",
-      handler: (request, h) => {
-         const {name = "Stranger"} = request.params;
-         const {lang} = request.query;
-
-         if (lang === "id") {
-            return `Halo ${name}!`;
-         }
-         if (lang === "jp") {
-            return `Ohayou ${name}!`;
-         }
-         if (lang === "fr") {
-            return `Bonjour ${name}!`
-         }
-         
-         return `Hello ${name}!`;
-      },
-   },
-
-   {
-      method: "POST",
-      path: "/login",
-      handler: (request, h) => {
-         const {username, password} = request.payload;
-         if (password === 123456) {
-            return `Welcome ${username}`;
-         }
-         return "Your password is wrong!";
-      },
-   },
-
-   {
-      method: "POST",
-      path: "/test",
-      handler: (request, h) => {
-         return h.response("Created")
-            .code(201)
-            .header("X-Powered-By", "NodeJS");
-      },
-   },
-
-   {
-      method: "*",
-      path: "/{any*}",
-      handler: (request, h) => {
-         return h.response("Halaman tidak ditemukan").code(404);
-      },
-   },
+      return `Hello ${name}!`;
+    },
+  },
+  {
+    method: 'POST',
+    path: '/login',
+    handler: (request, h) => {
+      const { username, password } = request.payload;
+      if (password === 123456) {
+        return `Welcome ${username}`;
+      }
+      return 'Your password is wrong!';
+    },
+  },
+  {
+    method: 'POST',
+    path: '/signup',
+    handler: (request, h) => {
+      const { username } = request.payload;
+      return h
+        .response({ status: 'success', message: `Welcome ${username}` })
+        .code(201)
+        .type('application/json')
+        .header('X-Powered-By', 'Hapi');
+    },
+  },
+  {
+    method: '*',
+    path: '/{any*}',
+    handler: (request, h) => {
+      return h.response('Halaman tidak ditemukan').code(404);
+    },
+  },
 ];
 
 export default routes;
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-import Hapi from "@hapi/hapi";
-import routes from "./routes.js";
+import Hapi from '@hapi/hapi';
+import routes from './routes.js';
 
 const init = async () => {
-   const server = Hapi.server({
-      port: 5000,
-      host: "localhost"
-   });
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
 
-   server.route(routes);
+  server.route(routes);
 
-   await server.start();
-   console.log(`Server berjalan pada ${server.info.uri}`);
-}
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
 
 init();
